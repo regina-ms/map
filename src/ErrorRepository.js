9f4dbbdb2667518c7d790945f7e44c05a8c14658
Regina <rmyachina@mail.ru>
2023-07-05 18:11:55 +0300

    fix class ErrorRepository

--- src/ErrorRepository.js
+++ src/ErrorRepository.js
@@ -1,6 +1,6 @@
 export default class ErrorRepository {
   constructor() {
-    this.errors = new Map();
+    this.errors = new Map([[687787, 'wrong format'], [558878, 'unknow-user']]);
   }
 
   translate(code) {
--- src/__tests__/ErrorRepository.test.js
+++ src/__tests__/ErrorRepository.test.js
@@ -7,11 +7,10 @@ test('should create a new ErrorRepository', () => {
 
 test('should return translate of error code', () => {
   const badErrors = new ErrorRepository();
-  badErrors.errors.set(687787, 'wrong format');
   expect(badErrors.translate(687787)).toBe('wrong format');
 });
 
 test('should return "Unknown error"', () => {
   const badErrors = new ErrorRepository();
-  expect(badErrors.translate(687787)).toBe('Unknown error');
+  expect(badErrors.translate(32589)).toBe('Unknown error');
 });
